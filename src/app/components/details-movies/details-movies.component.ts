387c46a34bb40c029fa06ec863196acfe0403030
Jonatan Santos <jonatan_santos_99@hotmail.com>
2025-03-24 20:38:20 -0300

    generating all components

---
+++ src/app/components/details-movies/details-movies.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-movies',
+  imports: [],
+  templateUrl: './details-movies.component.html',
+  styleUrl: './details-movies.component.css'
+})
+export class DetailsMoviesComponent {
+
+}
---
+++ src/app/components/details-reviews/details-reviews.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-reviews',
+  imports: [],
+  templateUrl: './details-reviews.component.html',
+  styleUrl: './details-reviews.component.css'
+})
+export class DetailsReviewsComponent {
+
+}
