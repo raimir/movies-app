f99b23c93aebc58251025682b3a05f8d75f8fc6d
raimir <jonatan_santos_99@hotmail.com>
2025-04-10 00:26:41 -0300

    footer - use ngDirective git push

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
+  imports: [CommonModule],
 })
 export class HomeComponent {
 
+  slideCarousel = [ 
+    {
+      title: 'First slide label',
+      description: 'Some representative placeholder content for the first slide.',
+      imageUrl: '/photo-1575783970733-1aaedde1db74.png',
+    },
+    {
+      title: 'Second slide label',
+      description: 'Some representative placeholder content for the first slide.',
+      imageUrl: '/photo-1575783970733-1aaedde1db74.png',
+    },
+    {
+      title: 'Third slide label',
+      description: 'Some representative placeholder content for the first slide.',
+      imageUrl: '/photo-1575783970733-1aaedde1db74.png',
+    },
+    {
+      title: 'Four slide label',
+      description: 'Some representative placeholder content for the first slide.',
+      imageUrl: '/photo-1575783970733-1aaedde1db74.png',
+    }
+  ]
 }
